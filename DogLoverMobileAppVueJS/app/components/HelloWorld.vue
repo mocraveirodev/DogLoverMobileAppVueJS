9d60570eefd78d76fe7ece7becca641004892ad4
Monica Craveiro de Menezes <craveiromonica@gmail.com>
2018-09-03 16:32:41 -0300

    NativeScript-Vue mobile app to display random pets

---
+++ DogLoverMobileAppVueJS/app/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+    <Page class="page">
+		<ActionBar title="So. Many. Dogs!" class="action-bar" />
+		<StackLayout class="card">
+			<Button class="btn" @tap="getADog">Find Me A Dog!</button>
+      <StackLayout class="placeholder">
+			<Image :src="dogImage.message" />
+		</StackLayout>	
+		</StackLayout>
+    </Page>
+</template>
+
+<script>
+const http = require("http");
+export default {
+    data() {
+    return {
+      dogImage: {}
+    }
+  },
+  methods: {
+    getADog() {
+      http.request({ url: "https://dog.ceo/api/breeds/image/random", method: "GET" }).then((response) => {
+        this.dogImage = JSON.parse(response.content)
+        console.log(this.dogImage.message)
+      }, (e) => {
+        alert("error")
+      });
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+.home-panel {
+    vertical-align: center;
+    font-size: 20;
+    margin: 15;
+}
+
+.description-label {
+    margin-bottom: 15;
+}
+</style>
